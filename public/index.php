9ce6f4739511e057745305fc841d91a02d817b91
3B232005 <3B232005@gm.student.ncut.edu.tw>
2025-11-17 02:19:37 +0800

    補充練習：新增 Monolog 測試程式碼，Logger 名稱 WISD，日誌寫入 log/my.log

--- public/index.php
+++ public/index.php
@@ -6,8 +6,19 @@ use Demo\Hello\Lara;
 use Demo\Hello;
 
 $world = new World();
-$lara= new Lara();
-$vincent= new Hello\Someone('Vincent');
-
+$lara = new Lara();
+$vincent = new Hello\Someone('Vincent');
 $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// 建立 logger
+$log = new Logger('WISD');
+
+// 指定 log 檔案路徑
+$log->pushHandler(new StreamHandler(__DIR__ . '/../log/my.log', Logger::DEBUG));
+
+// 寫入一筆測試訊息
+$log->info('這是 Monolog 的測試訊息！');
